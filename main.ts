0443c788dc9cc9ad5c5c2aa85bb97520a0b0bd14
dmorgun <daryamorgun@gmail.com>
2022-04-11 09:18:10 +0300

    Srarted tilemap

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 namespace SpriteKind {
     export const pet = SpriteKind.create()
 }
+tiles.setCurrentTilemap(tilemap`level1`)
+scene.setBackgroundImage(assets.image`background`)
 let cakebear = sprites.create(assets.image`cake bear`, SpriteKind.Player)
 controller.moveSprite(cakebear)
 let cake = sprites.create(assets.image`cake`, SpriteKind.pet)
