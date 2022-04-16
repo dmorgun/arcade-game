a8f9b23897b2f84fe669576ebb423914bb6f789a
dmorgun <daryamorgun@gmail.com>
2022-04-16 15:05:25 +0300

    Added music, pet follows bear, collect pinkcakes

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 namespace SpriteKind {
     export const pet = SpriteKind.create()
 }
-tiles.setCurrentTilemap(tilemap`level1`)
+scene.onOverlapTile(SpriteKind.Player, assets.tile`pinkcake`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`transparency16`)
+    cakebear.startEffect(effects.hearts)
+})
+let cakebear: Sprite = null
 scene.setBackgroundImage(assets.image`background`)
-let cakebear = sprites.create(assets.image`cake bear`, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level1`)
+cakebear = sprites.create(assets.image`cake bear`, SpriteKind.Player)
 controller.moveSprite(cakebear)
 let cake = sprites.create(assets.image`cake`, SpriteKind.pet)
+cakebear.setPosition(45, 100)
+cake.setPosition(55, 120)
+cake.follow(cakebear)
+scene.cameraFollowSprite(cakebear)
+music.playMelody("A F E F D G E F ", 140)
